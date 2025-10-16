b20b9db56a0a9d31591af7f386c46f3596235dcf
[dyad] <brouslight3@gmail.com>
2025-10-16 15:52:11 -0700

    [dyad] Undoing the last change - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,7 +1,16 @@
 export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
-      {/* Content removed as requested */}
+      <a
+        href="https://www.dyad.sh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:underline"
+      >
+        Made with Dyad
+      </a>
+      {" | "}
+      <span>Made by Levi Brous-Light</span>
     </div>
   );
 };
