8157cbfeb477600a36b4353b22aa9a4542a7643c
[dyad] <brouslight3@gmail.com>
2025-10-16 15:51:38 -0700

    [dyad] Removing 'Made with Dyad' text - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,16 +1,7 @@
 export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-gray-700 dark:hover:text-gray-200"
-      >
-        Made with Dyad
-      </a>
-      <span className="mx-2">|</span>
-      <span>Made by Levi Brous-Light</span>
+      {/* Content removed as requested */}
     </div>
   );
 };
