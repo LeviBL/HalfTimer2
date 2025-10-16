b1ca9afd561af22b26711e48d970cc0660bcf2a0
[dyad] <brouslight3@gmail.com>
2025-10-16 15:52:35 -0700

    [dyad] Removing 'Made with Dyad' link - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,15 +1,6 @@
 export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:underline"
-      >
-        Made with Dyad
-      </a>
-      {" | "}
       <span>Made by Levi Brous-Light</span>
     </div>
   );
