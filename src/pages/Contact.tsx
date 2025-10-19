070cf6c01bb38f5602b9baef8bf51582cdf7349a
[dyad] <brouslight3@gmail.com>
2025-10-18 20:11:24 -0700

    [dyad] Removed a paragraph from the Contact page - wrote 1 file(s)

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -23,10 +23,6 @@ const Contact: React.FC = () => {
             contacthalftimer@gmail.com
           </a>
         </p>
-
-        <p className="text-md text-gray-600 text-center italic">
-          We aim to respond to all inquiries within 2-3 business days.
-        </p>
       </div>
       <MadeWithDyad />
     </div>
