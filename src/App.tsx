29a4d2f224a6e2834b3c355b434ffd9b30d5bd9c
[dyad] <brouslight3@gmail.com>
2026-05-12 16:44:08 -0700

    [dyad] Hiding and cleaning up static SEO footer and content - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import HalfTimer from "./pages/HalfTimer";
 import NotFound from "./pages/NotFound";
-import React from "react";
+import React, { useEffect } from "react";
 import About from "./pages/About";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
@@ -19,35 +19,46 @@ import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <HelmetProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<HalfTimer />} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="/nba" element={<HalfTimer defaultSport="nba" />} />
-            <Route path="/nfl" element={<HalfTimer defaultSport="nfl" />} />
-            <Route path="/march-madness-halftime-timer" element={<HalfTimer defaultSport="ncaa" />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogPost />} />
-            <Route path="/blog/author/:authorName" element={<AuthorPosts />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsOfService />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </HelmetProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  useEffect(() => {
+    // Remove static SEO elements once React has taken over
+    const seoFooter = document.getElementById("seo-footer");
+    if (seoFooter) seoFooter.remove();
+    
+    const seoPlaceholder = document.getElementById("seo-content-placeholder");
+    if (seoPlaceholder) seoPlaceholder.remove();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<HalfTimer />} />
+              <Route path="/home" element={<Navigate to="/" replace />} />
+              <Route path="/nba" element={<HalfTimer defaultSport="nba" />} />
+              <Route path="/nfl" element={<HalfTimer defaultSport="nfl" />} />
+              <Route path="/march-madness-halftime-timer" element={<HalfTimer defaultSport="ncaa" />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blog/:slug" element={<BlogPost />} />
+              <Route path="/blog/author/:authorName" element={<AuthorPosts />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="/terms" element={<TermsOfService />} />
+              <Route path="/contact" element={<Contact />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
